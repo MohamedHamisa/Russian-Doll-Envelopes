0b824534040526078a11dee777ae4e8d7c92fbb2
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-03-14 21:15:13 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxEnvelopes(self, E):
+        E.sort(key=lambda x: (x[0], -x[1]))
+        dp = []
+        for _ , height in E:
+            left = bisect_left(dp, height)
+            if left == len(dp): dp.append(height)
+            else: dp[left] = height
+        return len(dp)
+
+
+
+#time complexity O(N log N).
